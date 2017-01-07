2a8b96371e4f38ac581862a2c9ffc49279af6265
Josh Benner <josh@bennerweb.com>
2017-01-07 16:46:50 -0500

    Initial import

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='flask_webmachine',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=find_packages(),
+
+    description='Webmachine-style resource framework for Flask',
+    author='Joshua Benner',
+    author_email='josh@bennerweb.com',
+    license='Apache',
+
+    install_requires=[
+        'six'
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov'
+        ]
+    }
+)
